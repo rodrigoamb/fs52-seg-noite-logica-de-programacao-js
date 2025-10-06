7e011e45a88b5f641d6facd802a2cec53e042b67
Rodrigo Medeiros <dev.rodrigomedeiros@gmail.com>
2025-10-06 20:54:04 -0300

    funcoes

--- funcoes.js
+++ funcoes.js
@@ -100,3 +100,53 @@ apresentacao({
   idade: 33,
   profissao: "Dentista",
 });
+
+console.log("--------------------------");
+
+// crie uma função que recebe um array de objetos. Esses objetos possui "nome" e "estoque" de carros no pátio da concessionária. Usando a função, mostre no console o nome dos carros e a quantidade de estoque deles.
+
+const carros = [
+  {
+    nome: "Corolla",
+    estoque: 4,
+  },
+  {
+    nome: "Civic",
+    estoque: 7,
+  },
+  {
+    nome: "Hr-v",
+    estoque: 3,
+  },
+];
+
+function mostraEstoque(carrosLista) {
+  console.log(
+    `Nome do carro: ${carrosLista[0].nome} -  Estoque: ${carrosLista[0].estoque} `
+  );
+  console.log(
+    `Nome do carro: ${carrosLista[1].nome} -  Estoque: ${carrosLista[1].estoque} `
+  );
+  console.log(
+    `Nome do carro: ${carrosLista[2].nome} -  Estoque: ${carrosLista[2].estoque} `
+  );
+}
+
+mostraEstoque(carros);
+
+// Uma concessionária deseja criar um pequeno sistema de controle de vendas usando JavaScript.
+// O objetivo é registrar os carros disponíveis em estoque e permitir registrar cada venda feita por um cliente, atualizando automaticamente o número de unidades restantes.
+
+// Crie o seguinte cenário:
+// 	1.	Declare um array de objetos chamado carros, onde cada objeto representa um carro com as propriedades:
+// 	•	nome: o modelo do carro (ex: "Corolla", "Civic", "Hr-v");
+// 	•	estoque: a quantidade disponível em estoque.
+// 	2.	Crie uma função nomeada chamada venda que receba três parâmetros:
+// 	•	nomeCliente → nome do cliente que está comprando;
+// 	•	index → posição do carro no array (ex: 0, 1, 2);
+// 	•	quantidade → quantidade de carros vendidos.
+// 	3.	Dentro da função:
+// 	•	Subtraia a quantidade vendida do estoque do carro correspondente;
+// 	•	Mostre no console a mensagem(usando template string):
+// Cliente: Rodrigo - Venda: 3 Civic
+// 4.	Após a execução da função, exiba no console o array atualizado, mostrando o novo estoque de cada carro.
