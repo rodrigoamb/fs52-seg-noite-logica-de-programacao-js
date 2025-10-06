26802ec2bc5fdd487d9a8d3895ddcc98b6955f21
Rodrigo Medeiros <dev.rodrigomedeiros@gmail.com>
2025-10-06 19:00:08 -0300

    funcoes

---
+++ funcoes.js
@@ -0,0 +1,29 @@
+//funções com JS
+
+//funcao que soma dois numeros
+
+//parametro da funcao
+
+function somaDoisNumeros(n1, n2) {
+  const resultado = n1 + n2;
+
+  console.log(resultado);
+}
+
+somaDoisNumeros(30, 20);
+somaDoisNumeros(100, 200);
+somaDoisNumeros(5, 3);
+somaDoisNumeros(2, 2);
+
+console.log("--------------------------");
+
+function multiplicaDoisNumero(n1, n2) {
+  const resultado = n1 * n2;
+
+  console.log(resultado);
+}
+
+multiplicaDoisNumero(3, 2);
+multiplicaDoisNumero(10, 2);
+multiplicaDoisNumero(33, 3);
+multiplicaDoisNumero(29, 7);
