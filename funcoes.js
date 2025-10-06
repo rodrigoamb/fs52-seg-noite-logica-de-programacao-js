078a89918ef1ceadd93c6629852abdcb7ea2dd18
Rodrigo Medeiros <dev.rodrigomedeiros@gmail.com>
2025-10-06 20:29:09 -0300

    funcoes

--- funcoes.js
+++ funcoes.js
@@ -1,4 +1,12 @@
-//funções com JS
+//funções com JS:
+
+//assuntos:
+// Funçoes nomeadas
+// função anônimas
+// arrow functions
+// execução da função (chamando a função)
+// parametros de função e argumentos
+// escopo global e escopo de bloco
 
 //funcao que soma dois numeros
 
@@ -58,11 +66,37 @@ console.log("--------------------------");
 //arrow functions
 
 const restoDaDivisaoDoisNumeros = (n1, n2) => {
-  const resultado = n1 % n2;
+  let resultado = 355;
 
   console.log(resultado);
 };
 
-restoDaDivisaoDoisNumeros(20, 2);
-restoDaDivisaoDoisNumeros(30, 3.5);
-restoDaDivisaoDoisNumeros(300, 45);
+// restoDaDivisaoDoisNumeros(10, 2);
+
+// console.log(resultado);
+
+// restoDaDivisaoDoisNumeros(20, 2);
+// restoDaDivisaoDoisNumeros(30, 3.5);
+// restoDaDivisaoDoisNumeros(300, 45);
+
+// Crie uma função nomeada que recebe como argumento um objeto pessoa e a função mostra no console uma apresentação dessa pessoa. Exemplo "Olá eu sou Tiago, tenho 20 anos e sou programador fullstack"
+
+const individuo = {
+  nome: "Alessandro",
+  idade: 25,
+  profissao: "Programador Backend",
+};
+
+function apresentacao(pessoa) {
+  console.log(
+    `Olá eu sou ${pessoa.nome}, tenho ${pessoa.idade} anos e sou ${pessoa.profissao}`
+  );
+}
+
+apresentacao(individuo);
+
+apresentacao({
+  nome: "Mariana",
+  idade: 33,
+  profissao: "Dentista",
+});
