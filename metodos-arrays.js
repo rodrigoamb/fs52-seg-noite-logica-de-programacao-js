d0b25a1cc532fce57e01ed22932fb0308f089f50
Rodrigo Medeiros <dev.rodrigomedeiros@gmail.com>
2025-10-27 21:33:56 -0300

    iniando metodos

---
+++ metodos-arrays.js
@@ -0,0 +1,15 @@
+// métodos de arrays
+
+// O que são métodos?
+// métodos são funções que existem dentro de um tipo objeto.
+
+// pessoa.nome -> nome é propriedade do objeto pessoa
+// console.log() -> log() é um método que existe dentro do objeto console
+
+const numeros = [34, 56, 78, 54];
+
+//adicionando o item no final do array - push()
+
+numeros.push(11);
+
+console.log(numeros);
