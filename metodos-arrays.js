9dbe812770fab102cbcd3572eb1b7483ebd7b3d5
Rodrigo Medeiros <dev.rodrigomedeiros@gmail.com>
2025-11-03 20:58:49 -0300

    metodos de arrays

--- metodos-arrays.js
+++ metodos-arrays.js
@@ -96,3 +96,76 @@ const venda1 = vendaDeCarro("civic", 3);
 console.log(venda1);
 
 const venda2 = vendaDeCarro("Fusca", 5);
+
+//filter() - Retorna um novo array apenas com os elementos que atendem a uma condição
+
+const pessoas = [
+  { firstName: "Rodrigo", lastName: "Medeiros", age: 35 },
+  { firstName: "Rodrigo", lastName: "Sampaio", age: 25 },
+  { firstName: "Ana", lastName: "Moura", age: 40 },
+];
+
+const arrayFiltrado = pessoas.filter((item) => {
+  return item.firstName === "Rodrigo";
+});
+
+console.log(arrayFiltrado);
+
+//forEach() - itero sobre cada elemento do array
+
+const lista = ["arroz", "feijão", "macarrão"];
+
+lista.forEach((item) => {
+  console.log(item);
+});
+
+//find() - retorna o primeiro elemento que satisfaz uma condição
+
+const numeros2 = [10, 20, 30, 40, 50, 60];
+
+const encontrados = numeros2.find((item) => {
+  return item > 40;
+});
+
+const encontrados2 = numeros2.filter((item) => {
+  return item >= 40;
+});
+
+console.log(encontrados);
+console.log(encontrados2);
+
+// some() = verifica se algum algum elemento do array atende a uma condição (true or false)
+
+const maiorQue100 = numeros2.some((item) => {
+  return item > 100;
+});
+
+console.log(maiorQue100);
+
+const maiorQue30 = numeros2.some((item) => {
+  return item > 30;
+});
+
+console.log(maiorQue30);
+
+//every() - Verifica se TODOS os elementos atendem a uma condição (true or false)
+
+const positivos = numeros2.every((item) => {
+  return item > 0;
+});
+console.log(positivos);
+
+numeros2.push(-5);
+
+const positivos2 = numeros2.every((item) => {
+  return item > 0;
+});
+
+console.log(positivos2);
+
+// exercicios:
+
+//1) Criar um array com alguns nomes e adicionar um novo nome no meio do array
+//2) Remover o primeiro nome do array
+//3) Criar um array de números e triplicar os valores dele
+//4) Criar um array de notas e retornar apenas as notas acima de 7
